4bf503c564d91fe5e1a4d01fd1044b4f619267bb
Jonatan Lledo <Jonatan.Lledo+ULVR@unilever.com>
2026-01-26 15:29:08 +0100

    fix import folder reference, again

--- blocks/media-text-cards/media-text-cards.js
+++ blocks/media-text-cards/media-text-cards.js
@@ -1,5 +1,4 @@
-import { createElement } from 'react';
-import { createVideoEmbed, findVideoLink } from '@scripts/common.js';
+import { createVideoEmbed, findVideoLink, createElement } from '@scripts/common.js';
 
 function openVideoModal(videoUrl) {
   const modalOverlay = createElement('div', { className: 'video-modal-overlay' });
