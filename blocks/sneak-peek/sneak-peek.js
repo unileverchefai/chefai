a378f19d3e4ebf451e2b8c805ed23d2a2a7528bf
KasraTab <kasra.tabrizi@netcentric.biz>
2026-02-04 12:26:13 +0100

    FECA-124: boilerplate

---
+++ blocks/sneak-peek/sneak-peek.js
@@ -0,0 +1,7 @@
+export default function decorate(block) {
+
+    const businessLogo = document.createElement('img');
+    const businessName = document.createElement('h3');
+    const sneakPeekText = document.createElement('p');
+    return block;
+};
